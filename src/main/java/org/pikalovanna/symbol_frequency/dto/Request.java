761fc9dc8c47726580b46ae33299496f5ceb9991
Pikalova Anna <g2012.pikalova.a.12176@gmail.com>
2023-09-13 17:12:21 +0300

    add: text message for validation

--- src/main/java/org/pikalovanna/symbol_frequency/dto/Request.java
+++ src/main/java/org/pikalovanna/symbol_frequency/dto/Request.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class Request {
-    @NotBlank
-    @Pattern(regexp = "^[a-z]{1,2147483647}+$")
+    @NotBlank(message = "Поле не может быть пустым")
+    @NotNull(message = "Поле не может быть пустым")
+    @Pattern(regexp = "^[a-z]{1,2147483647}+$", message = "Поле содержит недопустимые символы")
     public String text;
 }
